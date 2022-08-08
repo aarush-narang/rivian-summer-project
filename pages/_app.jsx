b6d6ac3221193d89a0fb076028b2d5126525f0a1
aarush-narang <aarushnarang@gmail.com>
2022-08-08 13:48:31 -0700

    build(workflows): [riv-1]: Initialized CI yml

---
+++ pages/_app.jsx
@@ -0,0 +1,20 @@
+import '../styles/globals.css';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import PropTypes from 'prop-types';
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <ParallaxProvider>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <Component {...pageProps} />
+        </ParallaxProvider>
+    );
+}
+
+MyApp.propTypes = {
+    Component: PropTypes.node.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    pageProps: PropTypes.any.isRequired,
+};
+
+export default MyApp;
