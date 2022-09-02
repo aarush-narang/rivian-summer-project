b13f6afe213aeaaef7ed8cf395117313f6789d6c
aarush-narang <aarushnarang@gmail.com>
2022-09-02 16:39:37 -0700

    feat(sitemap): [riv-4]: Added sitemap docs link

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,4 @@
+// https://www.npmjs.com/package/next-sitemap
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.NEXT_SITE_DOMAIN_ROOT || 'https://aarush-narang.github.io/rivian-summer-project',
