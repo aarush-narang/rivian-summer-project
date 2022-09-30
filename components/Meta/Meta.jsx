c4efddf59712f91c54388ed90f2e437a1738966d
aarush-narang <aarushnarang@gmail.com>
2022-09-30 16:45:30 -0700

    fix dangerouslySetHTML for GA tag

--- components/Meta/Meta.jsx
+++ components/Meta/Meta.jsx
@@ -33,7 +33,7 @@ export default function MetaTags({
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-46N8RCD9HT" />
 
             <script dangerouslySetInnerHTML={{
-                html: `window.dataLayer = window.dataLayer || [];
+                __html: `window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                         gtag('config', 'G-46N8RCD9HT');`,
